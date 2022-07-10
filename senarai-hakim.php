3797530f05054d606455c24979c490c7818fb360
Suriagajan <suriasiva27@gmail.com>
2022-07-10 17:22:26 +0800

    finished senarai hakim.php

--- senarai-hakim.php
+++ senarai-hakim.php
@@ -7,20 +7,21 @@ include('header.php');
 include('guard-hakim.php');
 include('connection.php');
 ?>
-
-<h3>SENARAI HAKIM</h3>
+<div class="senaraiHakim">
+<h3 id="senaraiHakim-title">SENARAI HAKIM</h3>
 <!--memanggil fail butang-saiz-->
 <?php include('butang-saiz.php'); ?>
 
 <!--Navigasi untuk mendaftar hakim baru-->
-<a href="hakim-daftar-borang.php">[+] Daftar Hakim Baru</a>
-
+<div class="daftarHakim-btn">
+<a href="hakim-daftar-borang.php">Daftar Hakim Baru</a>
+</div>
 <!--Header bagi jadual untuk memaparkan senarai peserta--> 
 <table width="100%" border="1"  id="saiz">
     <tr>
-        <td>Nama</td>
-        <td>No KP</td>
-        <td>katalaluan</td>
+        <td>NAMA</td>
+        <td>NOKP</td>
+        <td>KATALALUAN</td>
     </tr>
 
 <?php
@@ -42,4 +43,7 @@ $laksana = mysqli_query($condb,$arahan_papar);
     }
 ?>
 </table>
+<div class="senaraiHakim-footer">
 <?php include('footer.php'); ?>
+</div>
+</div>
